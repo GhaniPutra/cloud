3f7701528058da65b1e6da1f2db23136638cee44
Rio Store <93362148+GhaniPutra@users.noreply.github.com>
2021-12-31 17:57:48 +0700

    Add files via upload

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php 
+$OC_Version = array(10,9,0,3);
+$OC_VersionString = '10.9.0';
+$OC_Channel = 'stable';
+$OC_Edition = 'community';
+$OC_VersionCanBeUpgradedFrom = [[8,2,11],[9,0,9],[9,1]];
+$OC_Build = '2021-12-20T21:32:07+00:00 d5453e81e5398501b808b2c815758e6f6c02196e';
+$vendor = 'owncloud';
